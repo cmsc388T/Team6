86e1a75295d737cb5b4e969a7a9992549ef8d698
harmless-tech <28830529+harmless-tech@users.noreply.github.com>
2021-01-06 16:32:16 -0500

    added testing for map eat cookie method

--- Projects/P1/tests/TestMapEatCookie.java
+++ Projects/P1/tests/TestMapEatCookie.java
@@ -2,9 +2,28 @@ import junit.framework.*;
 import java.awt.Color;
 import java.io.*;
 
-public class TestMapEatCookie {
+public class TestMapEatCookie extends TestCase {
 	
-	public void testMapEatCookie() {
-		return null;
+	private static final int MAX_X = 25;
+	private static final int MAX_Y = 24;
+	private static final int MAX_COOKIES = 263;
+	
+	public void testMapEatCookie() throws FileNotFoundException {
+		NoFrame frame = new NoFrame();
+		Map map = frame.getMap();
+		
+		System.out.println("Cookies: " + map.getCookies());
+		
+		int consumedCookies = 0;
+		for (int x = 0; x < MAX_X; x++) {
+			for (int y = 0; y < MAX_Y; y++) {
+				if (map.eatCookie("tok_x" + x + "_y" + y) != null)
+					consumedCookies++;
+			}
+		}
+		
+		System.out.println("Cookies: " + consumedCookies);
+		
+		assertTrue(consumedCookies == MAX_COOKIES);
 	}
 }
