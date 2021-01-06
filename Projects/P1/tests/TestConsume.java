849630771f5b823a71556b2c86b23987282399ae
harmless-tech <28830529+harmless-tech@users.noreply.github.com>
2021-01-06 15:39:20 -0500

    finished tests for method consume

--- Projects/P1/tests/TestConsume.java
+++ Projects/P1/tests/TestConsume.java
@@ -4,7 +4,25 @@ import java.io.*;
 
 public class TestConsume extends TestCase {
 
+	private static final int MAX_X = 25;
+	private static final int MAX_Y = 24;
+	private static final int MAX_COOKIES = 263;
+	
 	public void testConsume() throws FileNotFoundException {
-		return null;	
+		NoFrame frame = new NoFrame();
+		PacMan pacman = frame.addPacMan(new Location(1, 1));
+		
+		int consumedCookies = 0;
+		for (int x = 0; x < MAX_X; x++) {
+			for (int y = 0; y < MAX_Y; y++) {
+				pacman.myLoc.x = x;
+				pacman.myLoc.y = y;
+				
+				if (pacman.consume() != null)
+					consumedCookies++;
+			}
+		}
+		
+		assertTrue(consumedCookies == MAX_COOKIES);
 	}
 }
