000b3fe17a6bb30b61c89e7d83e51f636f8b8aa7
Samuel Markey <sammarkey1999@gmail.com>
2021-01-06 23:28:02 -0500

    Implemented Pacman-move

--- Projects/P1/src/PacMan.java
+++ Projects/P1/src/PacMan.java
@@ -2,11 +2,13 @@ import java.util.HashSet;
 import java.util.ArrayList;
 import javax.swing.JComponent;
 
+import com.sun.jdi.Location;
+
 public class PacMan{
 	String myName;
 	Location myLoc;
 	Map myMap;
-	Location shift; 
+	Location shift;
 
 	public PacMan(String name, Location loc, Map map) {
 		this.myLoc = loc;
@@ -15,18 +17,27 @@ public class PacMan{
 	}
 
 	public ArrayList<Location> get_valid_moves() {
-		return null;	
+		return null;
 	}
 
 	public boolean move() {
+		ArrayList<Location> moves = get_valid_moves();
+		// Check for at least a single valid move
+		if(!moves.isEmpty()) {
+			// Pick the first one, easily changeable
+			myLoc = moves.get(0);
+			return true;
+		} else {
+			// Pacman cannot move!
+			return false;
+		}
+	}
+
+	public boolean is_ghost_in_range() {
 		return false;
 	}
 
-	public boolean is_ghost_in_range() { 
-		return false;
-	}
-
-	public JComponent consume() { 
+	public JComponent consume() {
  		return null;
 	}
 }
