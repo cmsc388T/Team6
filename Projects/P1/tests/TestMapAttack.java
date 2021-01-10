62752914818702f63ca947e801f655fc7fd36966
jjiang14 <jjiang14@terpmail.umd.edu>
2021-01-10 16:59:32 -0500

    Implemented testMapAttack. Note that I relied on the parameters of PacManComponent and GhostComponent to test functionality, since Map has no access to the actual PacMan and Ghost instances.

--- Projects/P1/tests/TestMapAttack.java
+++ Projects/P1/tests/TestMapAttack.java
@@ -4,6 +4,16 @@ import junit.framework.*;
 public class TestMapAttack {
 
 	public void testMapAttack() {
-		return null;	
+		NoFrame frame = new NoFrame();
+		
+		// Can't attack
+		PacMan pacman = frame.addPacMan(new Location(0, 0));
+		Ghost ghost = frame.addGhost(new Location (5, 5), "clyde", Color.orange);
+		assertFalse(frame.getMap().attack("clyde"));
+		
+		// Can attack
+		frame.getMap().add("blinky", new Location(1, 1),
+		new GhostComponent(1, 1, 20), Map.Type.GHOST);
+		assertTrue(frame.getMap().attack("blinky"));
 	}
 }
