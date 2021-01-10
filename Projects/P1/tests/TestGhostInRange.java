e3ced886b3cc0b80656320caac5369212392e6ff
jjiang14 <jjiang14@terpmail.umd.edu>
2021-01-10 16:32:49 -0500

    Implemented testGhostInRange

--- Projects/P1/tests/TestGhostInRange.java
+++ Projects/P1/tests/TestGhostInRange.java
@@ -5,7 +5,33 @@ import java.io.*;
 public class TestGhostInRange extends TestCase {
 
 	public void testGhostInRange() throws FileNotFoundException{
-		return null;
+		NoFrame frame = new NoFrame();
+		
+		// Not in range
+		PacMan pacman = frame.addPacMan(new Location(0, 0));
+		Ghost ghost = frame.addGhost(new Location(5, 5), "clyde", Color.orange);
+		assertFalse(ghost.is_pacman_in_range());
+		
+		// Vertical
+		pacman.myLoc.x = 6;
+		pacman.myLoc.y = 6;
+		ghost.myLoc.x = 6;
+		ghost.myLox.y = 5;
+		assertTrue(ghost.is_pacman_in_range());
+		
+		// Horizontal
+		pacman.myLoc.x = 6;
+		pacman.myLoc.y = 6;
+		ghost.myLoc.x = 5;
+		ghost.myLox.y = 6;
+		assertTrue(ghost.is_pacman_in_range());
+		
+		// Diagonal
+		pacman.myLoc.x = 6;
+		pacman.myLoc.y = 6;
+		ghost.myLoc.x = 5;
+		ghost.myLox.y = 5;
+		assertTrue(ghost.is_pacman_in_range());
 	}
 }
 
