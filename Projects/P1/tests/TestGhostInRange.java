7da75be3900eda07c2160bcd5f36db4a86985053
jjiang14 <76969931+jjiang14@users.noreply.github.com>
2021-01-11 23:15:20 -0500

    Fixed syntax errors (#49)
    * Fixed syntax errors
    
    * Made changes to TestGhostInRange.java

--- Projects/P1/tests/TestGhostInRange.java
+++ Projects/P1/tests/TestGhostInRange.java
@@ -16,21 +16,27 @@ public class TestGhostInRange extends TestCase {
 		pacman.myLoc.x = 6;
 		pacman.myLoc.y = 6;
 		ghost.myLoc.x = 6;
-		ghost.myLox.y = 5;
+		ghost.myLoc.y = 5;
+		frame.getMap().move("pacman", pacman.myLoc, Map.Type.PACMAN);
+		frame.getMap().move("clyde", ghost.myLoc, Map.Type.GHOST);
 		assertTrue(ghost.is_pacman_in_range());
 		
 		// Horizontal
 		pacman.myLoc.x = 6;
 		pacman.myLoc.y = 6;
 		ghost.myLoc.x = 5;
-		ghost.myLox.y = 6;
+		ghost.myLoc.y = 6;
+		frame.getMap().move("pacman", pacman.myLoc, Map.Type.PACMAN);
+		frame.getMap().move("clyde", ghost.myLoc, Map.Type.GHOST);
 		assertTrue(ghost.is_pacman_in_range());
 		
 		// Diagonal
 		pacman.myLoc.x = 6;
 		pacman.myLoc.y = 6;
 		ghost.myLoc.x = 5;
-		ghost.myLox.y = 5;
+		ghost.myLoc.y = 5;
+		frame.getMap().move("pacman", pacman.myLoc, Map.Type.PACMAN);
+		frame.getMap().move("clyde", ghost.myLoc, Map.Type.GHOST);
 		assertTrue(ghost.is_pacman_in_range());
 	}
 }
