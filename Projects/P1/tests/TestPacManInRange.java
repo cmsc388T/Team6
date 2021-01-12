14ef114faa8b72bb78fb3b5a5de47ce65e0ca7a8
Alejandro Rigau <76959275+arigau@users.noreply.github.com>
2021-01-11 23:31:30 -0500

    Ftr/pacman Final (#55)
    * finished method consume
    
    * finished tests for method consume
    
    * changed how cookie is checked for
    
    * Implemented is_ghost_in_range
    
    * Made some changes to is_pacman_in_range
    
    * Included null checks in is_ghost_in_range
    
    * Implemented testPacManInRange
    
    * Removed 'return null' from testPacManInRange implementation
    
    * Removed checks of diagonals
    
    * Restored checks on diagonals
    
    * Get valid moves pacman function and test (#26)
    
    * prototype - further testing needed
    
    * added diagonal and test cases
    
    * removed the start game to avoid issues
    
    Co-authored-by: Alejandro rigau <arigau@terpmail.umd.edu>
    
    * FTR/Pacman/move (#20)
    
    * Implemented Pacman-move
    
    * added null checking for movelist
    
    * added randomization to movement and testing
    
    Co-authored-by: harmless-tech <28830529+harmless-tech@users.noreply.github.com>
    
    * Pacman move (#41)
    
    * Implemented Pacman-move
    
    * added null checking for movelist
    
    * added randomization to movement and testing
    
    * added map.move fix
    
    Co-authored-by: harmless-tech <28830529+harmless-tech@users.noreply.github.com>
    
    * fix to map type (#43)
    
    * Implemented Pacman-move
    
    * added null checking for movelist
    
    * added randomization to movement and testing
    
    * added map.move fix
    
    * type fix for pacman
    
    Co-authored-by: harmless-tech <28830529+harmless-tech@users.noreply.github.com>
    
    * Made changes to TestPacManInRange.java (#51)
    
    Co-authored-by: harmless-tech <28830529+harmless-tech@users.noreply.github.com>
    Co-authored-by: jjiang14 <jjiang14@terpmail.umd.edu>
    Co-authored-by: Alejandro rigau <arigau@terpmail.umd.edu>
    Co-authored-by: SamuelMarkey <64279594+SamuelMarkey@users.noreply.github.com>
    Co-authored-by: jjiang14 <76969931+jjiang14@users.noreply.github.com>

--- Projects/P1/tests/TestPacManInRange.java
+++ Projects/P1/tests/TestPacManInRange.java
@@ -5,6 +5,37 @@ import java.io.*;
 public class TestPacManInRange extends TestCase {
 
 	public void testPacManInRange() throws FileNotFoundException{
-		return null;
+		NoFrame frame = new NoFrame();
+		
+		PacMan pacman = frame.addPacMan(new Location (0, 0));
+		Ghost ghost = frame.addGhost(new Location(2, 2), "clyde", Color.orange);
+		assertFalse(pacman.is_ghost_in_range());
+		
+		// Vertical
+		pacman.myLoc.x = 5;
+		pacman.myLoc.y = 5;
+		ghost.myLoc.x = 5;
+		ghost.myLoc.y = 6;
+		frame.getMap().move("pacman", pacman.myLoc, Map.Type.PACMAN);
+		frame.getMap().move("clyde", ghost.myLoc, Map.Type.GHOST);
+		assertTrue(pacman.is_ghost_in_range());
+		
+		// Horizontal
+		pacman.myLoc.x = 5;
+		pacman.myLoc.y = 5;
+		ghost.myLoc.x = 6;
+		ghost.myLoc.y = 5;
+		frame.getMap().move("pacman", pacman.myLoc, Map.Type.PACMAN);
+		frame.getMap().move("clyde", ghost.myLoc, Map.Type.GHOST);
+		assertTrue(pacman.is_ghost_in_range());
+		
+		// Diagonal
+		pacman.myLoc.x = 5;
+		pacman.myLoc.y = 5;
+		ghost.myLoc.x = 6;
+		ghost.myLoc.y = 6;
+		frame.getMap().move("pacman", pacman.myLoc, Map.Type.PACMAN);
+		frame.getMap().move("clyde", ghost.myLoc, Map.Type.GHOST);
+		assertTrue(pacman.is_ghost_in_range());
 	}
 }
