08b9e1987cc7816d69279c9362a3aba37c07c610
Alejandro rigau <arigau@terpmail.umd.edu>
2021-01-16 00:33:41 -0500

    sabotaged all my functions

--- Projects/P1/src/Ghost.java
+++ Projects/P1/src/Ghost.java
@@ -16,7 +16,7 @@ public class Ghost{
 		ArrayList<Location> newLoc = new ArrayList<Location>();
 		
 		//cartesian
-		if(!this.myMap.getLoc(this.myLoc.shift(1,0)).contains(Map.Type.WALL)){
+		if(this.myMap.getLoc(this.myLoc.shift(1,0)).contains(Map.Type.WALL)){
 			newLoc.add(this.myLoc.shift(1,0));
 		}
 		if(!this.myMap.getLoc(this.myLoc.shift(-1,0)).contains(Map.Type.WALL)){
